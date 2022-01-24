0399992b019b64167fdb6a0e4c03179716917270
vermei2@outlook.com <vermei2@outlook.com>
2022-01-24 21:16:11 +0300

    Добавьте файлы проекта.

---
+++ Lab16_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Lab16_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonString = String.Empty;
+            using (StreamReader sr = new StreamReader("../../../products.json"))
+            {
+                jsonString = sr.ReadToEnd();
+            }
+            Product[] products = JsonSerializer.Deserialize<Product[]>(jsonString);
+
+            Product maxProduct = products[0];
+            foreach (Product e in products)
+            {
+                if (e.Price > maxProduct.Price)
+                {
+                    maxProduct = e;
+                }
+            }
+            Console.WriteLine($"{maxProduct.Kod} {maxProduct.Name} {maxProduct.Price}");
+            Console.ReadKey();
+        }
+    }
+}
